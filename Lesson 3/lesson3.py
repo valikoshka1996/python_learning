de3bd1a107cceff7542ad4e5d43a120b8bc9513f
valikoshka1996 <valikoshka1996@gmail.com>
2024-09-02 10:14:52 +0300

    Second commit

---
+++ Lesson 3/lesson3.py
@@ -0,0 +1,17 @@
+#VARIABLES
+
+#BASIC VARIABLE PRINT
+num = 5
+print(num)
+num = 7
+print(num)
+# del num
+# print(num)
+
+print("Your number:", num)
+
+#VAR TYPES
+
+numreal = 14.48
+word = "Hello Word"
+boolean = False
