ed10364854e6be090a94bdc7841af8038c2e6fbf
valikoshka1996 <valikoshka1996@gmail.com>
2024-09-02 09:55:12 +0300

    Initial commit

---
+++ Lesson2/lesson2.py
@@ -0,0 +1,30 @@
+# PRINT STRING
+print("Hello World")
+print(5)
+print(5.5)
+print(5,"Hello World")
+print("\n")
+print("\t", 5, 6, 7, sep=", ")
+print("\n\n\n")
+print(5, 6, 7, sep=", ", end=".")
+print('Jonh\'s book')
+print('Jonh\\s book')
+
+#MATH
+
+print(5 + 5) #PLUS
+print(5 / 5) 
+print(5 % 5) #MOD
+print (2 ** 3)
+print(min(3, 5, 0, 1))
+print(max(3, 5, 0, 1))
+print(abs(-10))
+print(pow(2, 3)) #min() max() abs() pow() 
+print(round(3,4))
+
+#INPUT
+
+input("Enter Number")
+
+
+
