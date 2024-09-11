bed384e792741856a1ac7565390992c764ace9e4
v_bass_ <valikoshka1996@gmail.com>
2024-09-11 14:23:16 +0300

    Lesson 11

---
+++ Lesson 11/Lesson11.py
@@ -0,0 +1,19 @@
+#FUNCTIONS
+
+def myFunction(word):
+	print(word, end="!")
+	print("!!!")
+
+
+def summ(a, b):
+	res = float(a) + float(b)
+	return(res)
+
+
+
+a = input("Enter A:")
+b = input("Enter B:")
+result = summ(a, b)
+
+print(result)
+
---
+++ Lesson 11/Lesson11_1.py
@@ -0,0 +1,26 @@
+def minimal(l):
+	minX = l[0]
+	for i in l:
+		if i < minX:
+			minX = i
+	
+	return(minX)
+
+
+
+nums1 = [3, -222, 5, 2, 23, 53, 34, 34, 2332, 2, 6, 3, 34, 12]
+nums2 = [23, 2, 12, 5, 34, -9, 6, 23, 11, -323]
+
+minimal1 = minimal(nums1)
+minimal2 = minimal(nums2)
+
+if minimal1 < minimal2:
+	print(minimal1)
+else:
+	print(minimal2)
+
+
+#LAMBDA FUNC
+func = lambda x, y: x * y
+
+print(func(3, 2))
