057cc4ecfd205163d316d59af1d1025264427f81
valikoshka1996 <valikoshka1996@gmail.com>
2024-09-19 11:21:13 +0000

    Lesson 18 Update

--- Lesson 18/Lesson18.py
+++ Lesson 18/Lesson18.py
@@ -1,1 +1,38 @@
-print("Hello World")
+class Build:
+
+	__year = None
+	__city = None
+
+	def __init__(self, year, city):
+		self.year = year
+		self.city = city
+
+	def getInfo(self):
+		return self.year, self.city
+
+class School(Build):
+
+	pupils = None
+
+	def __init__(self, year, city, pupils=500):
+		super(School, self).__init__(year, city)
+		self.pupils = pupils
+
+	def getInfo(self):
+		data = super().getInfo()
+		return self.pupils, data
+
+
+class House(Build):
+	pass
+
+
+class Shop(Build):
+	pass
+
+
+school = School(2000, "Kiev", 3000)
+school.__year = 900
+house = House(1990, "Miami")
+shop = Shop(1800, "Khmelnitsky")
+print(school.getInfo())
